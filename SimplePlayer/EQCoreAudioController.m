3cc909ca2856bf1226ea77c7c1c5e4021ce55545
Daniel Kennett <dan@spotify.com>
2012-04-03 10:59:13 +0200

    Tweaks + license.

--- SimplePlayer/EQCoreAudioController.m
+++ SimplePlayer/EQCoreAudioController.m
@@ -3,9 +3,32 @@
 //  SimplePlayer with EQ
 //
 //  Created by Daniel Kennett on 02/04/2012.
-//  Copyright (c) 2012 Spotify. All rights reserved.
-//
-
+/*
+ Copyright (c) 2011, Spotify AB
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * Neither the name of Spotify AB nor the names of its contributors may 
+ be used to endorse or promote products derived from this software 
+ without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL SPOTIFY AB BE LIABLE FOR ANY DIRECT, INDIRECT,
+ INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+ LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+ OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 #import "EQCoreAudioController.h"
 
 @implementation EQCoreAudioController {
@@ -27,6 +50,8 @@
 #pragma mark - Setting up the EQ
 
 -(BOOL)connectOutputBus:(UInt32)sourceOutputBusNumber ofNode:(AUNode)sourceNode toInputBus:(UInt32)destinationInputBusNumber ofNode:(AUNode)destinationNode inGraph:(AUGraph)graph error:(NSError **)error {
+	
+	// Override this method to connect the source node to the destination node via an EQ node.
 	
 	// A description for the EQ Device
 	AudioComponentDescription eqDescription;
@@ -63,9 +88,6 @@
         NSLog(@"[%@ %@]: %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd), @"Couldn't set EQ parameter");
         return NO;
     }
-
-	
-	// Connect the sourceNode to the destinationNode via an EQ node. 
 	
 	// Connect the output of the source node to the input of the EQ node
 	status = AUGraphConnectNodeInput(graph, sourceNode, sourceOutputBusNumber, eqNode, 0);
@@ -74,7 +96,7 @@
         return NO;
     }
 	
-	// Connect the output of the EQ node to the input of the destination node.
+	// Connect the output of the EQ node to the input of the destination node, thus completing the chain.
 	status = AUGraphConnectNodeInput(graph, eqNode, 0, destinationNode, destinationInputBusNumber);
 	if (status != noErr) {
         NSLog(@"[%@ %@]: %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd), @"Couldn't connect eq to output");
